faf44f90fa23599e28f9c67397924f834ba80b47
Keyan Pishdadian <kpishdadian@gmail.com>
2014-11-30 14:56:33 -0500

    comments and timing changes
    added some comments, increased accepted range of midpoint, decreased
    servo movement step to prevent endless overcompensation loops

--- face_detector.py
+++ face_detector.py
@@ -3,13 +3,27 @@ This script uses the OpenCV Haar Cascades to detect faces. Input is taken from a
 Due to the preloaded classifier being used, only frontal face recognition is currently functional.
 
 http://docs.opencv.org/trunk/doc/py_tutorials/py_objdetect/py_face_detection/py_face_detection.html
+
+The location of faces is output to an Arduino, which uses servos to adjust webcam position in order to center the face in
+the window.
 """
 import sys #for webcam access
 import cv2 #openCV library
 import serial #allows Arduino interfacing
 import numpy
-import time
+import time #for delays
 
+"""
+Takes as parameters the xy and height width position of the detected face, finds where the middle of the window is and sends
+characters to the Arduino which direct it to:
+u: move up
+d: move down
+l: move left
+r: move right
+
+A delay is used so that the servos don't overshoot the midpoint and go into an overcompensation loop. This is also the reason
+why the midpoint is allowed to be within +- 100 pixels.
+"""
 def adjust_camera(x, y, w, h):
     nothing = 0
     mid_y = int(y + (h / 2.0))
@@ -17,11 +31,10 @@ def adjust_camera(x, y, w, h):
     
     if mid_y == half_height:
         nothing = 1
-        print "nothing y"
-    elif mid_y < (half_height) - 100:
+    elif mid_y < (half_height) - 125: #if the current location of the face is more than 100 pixels away from the center
         ser.write('u')
         print 'u'
-    elif mid_y > (half_height) + 100:
+    elif mid_y > (half_height) + 125: #if the current location of the face is more than 100 pixels away from the center
         ser.write('d')
         print 'd'
     
@@ -29,18 +42,15 @@ def adjust_camera(x, y, w, h):
     
     if mid_x == half_width:
         nothing = 1
-        print "nothing x"
-    elif mid_x < (half_width) - 100:
+    elif mid_x < (half_width) - 125: #if the current location of the face is more than 100 pixels away from the center
         ser.write('l')
         print 'l'
-    elif mid_x > (half_width) + 100:
+    elif mid_x > (half_width) + 125: #if the current location of the face is more than 100 pixels away from the center
         ser.write('r')
         print 'r'
     
-    print mid_y
-    print mid_x
-    print half_height
-    print half_width
+    print "Current Face height is:", mid_y
+    print "Current Face width is:", mid_x
     time.sleep(0.005)
         
 #cascadePath = sys.argv[0]
@@ -52,8 +62,8 @@ ser = serial.Serial('/dev/cu.usbmodem411', 9600) #set up serial port to interfac
 # video_stream.set(4, 400) #propID for the alias CV_CAP_PROP_FRAME_HEIGHT
 sizew = video_stream.get(3) #propID for the alias CV_CAP_PROP_FRAME_WIDTH
 sizeh = video_stream.get(4) #propID for the alias CV_CAP_PROP_FRAME_HEIGHT
-half_height = int(sizeh / 2.0)
-half_width = int(sizew / 2.0)
+half_height = int(sizeh / 2.0) #determine window midpoint
+half_width = int(sizew / 2.0) #determine window midpoint, used for the adjust_camera method
 
 #continously take new frames and convert to grayscale, then
 while True:
@@ -63,11 +73,12 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5) #detect face, scale adjustment allows detection of different size faces
     for (x, y, w, h) in faces: #draw rectangle around faces
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        print x, y #display xy coordinates of the center of the face, will be output to arduino
-        adjust_camera(x, y, w, h)
+        print "Current Face XY location is:", x, y #display xy coordinates of the center of the face
+        adjust_camera(x, y, w, h) #adjust webcam location based on current face position
     cv2.imshow('Video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    #time.sleep(0.005)
 
 video_capture.release()
 cv2.destroyAllWindows()
--- servo_controller/servo_controller.ino
+++ servo_controller/servo_controller.ino
@@ -4,38 +4,38 @@ Servo pan;
 Servo tilt;
 
 void setup() {
-  tilt.attach(3);
-  pan.attach(4);
-  tilt.write(70);
-  pan.write(65);
-  Serial.begin(9600);
+  tilt.attach(3); //tilt servo attached to pin 3
+  pan.attach(4); // pan servo attached to pin 4
+  tilt.write(70); //set intial position of tilt servo to 70
+  pan.write(65); //set initial position of pan servo to 65
+  Serial.begin(9600); //Serial port which will have recieved chars from Python
   pinMode(13, OUTPUT);
 }
 
 void loop(){
-  short pos;
+  short pos; //hold position of servo
   
-  digitalWrite(13, HIGH);
+  digitalWrite(13, HIGH); //random LED for coolness factor
   
   switch (Serial.read()){
    case 'u':
    case 'k':
      pos = tilt.read();
-     if (pos > 0) tilt.write(pos - 8);
+     if (pos > 0) tilt.write(pos - 5);
      break;
    case 'd':
    case 'j':
      pos = tilt.read();
-     if (pos < 180) tilt.write(pos + 8);
+     if (pos < 180) tilt.write(pos + 5);
      break;
    case 'l':
      pos = pan.read();
-     if (pos > 0) pan.write(pos - 8);
+     if (pos > 0) pan.write(pos - 5);
      break;
    case 'r':
    case 'h':
      pos = pan.read();
-     if (pos < 180) pan.write(pos + 8);
+     if (pos < 180) pan.write(pos + 5);
      break;
   }
   
